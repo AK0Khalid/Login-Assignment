c5594f51d2cbcc541b6eff319907a808b438b98d
AK0Khalid <ak659972@gmail.com>
2024-11-23 16:47:57 +0200

    assignment

---
+++ login.js
@@ -0,0 +1,86 @@
+
+
+var signupName = document.getElementById('signupName')
+var signupEmail = document.getElementById('signupEmail')
+var signupPassword = document.getElementById('signupPassword')
+var signinEmail = document.getElementById('signinEmail')
+var signinPassword = document.getElementById('signinPassword')
+  
+
+
+var pathparts = location.pathname.split('/');
+var baseURL = ''
+for (var i = 0; i < pathparts.length - 1; i++) {
+    baseURL += '/' + pathparts[i]
+}
+console.log(baseURL);
+
+var username = localStorage.getItem('sessionUsername')
+if (username) {
+    document.getElementById('username').innerHTML = "Welcome! " + username
+}
+
+var signUpArray = []
+if (localStorage.getItem('users') == null) {
+    signUpArray = []
+} else {
+    signUpArray = JSON.parse(localStorage.getItem('users'))
+}
+
+
+
+function isEmpty() {
+
+    if (signupName.value == "" || signupEmail.value == "" || signupPassword.value == "") {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+function isEmailExist() {
+    for (var i = 0; i < signUpArray.length; i++) {
+        if (signUpArray[i].email.toLowerCase() == signupEmail.value.toLowerCase()) {
+            return false
+        }
+    }
+}
+
+
+
+
+
+function signUp() {
+    if (isEmpty() == false) {
+        document.getElementById('exist').innerHTML = '<span class="text-danger m-3">All inputs is required</span>'
+        return false
+    }
+    
+    
+    var signUp = {
+        name: signupName.value,
+        email: signupEmail.value,
+        password: signupPassword.value,
+    }
+    if (signUpArray.length == 0) {
+        signUpArray.push(signUp)
+        localStorage.setItem('users', JSON.stringify(signUpArray))
+        document.getElementById('exist').innerHTML = '<span class="text-success m-3">Success</span>'
+        return true
+    }
+    if (isEmailExist() == false) {
+        document.getElementById('exist').innerHTML = '<span class="text-danger m-3">email already exists</span>'
+
+    } else {
+        signUpArray.push(signUp)
+        localStorage.setItem('users', JSON.stringify(signUpArray))
+        document.getElementById('exist').innerHTML = '<span class="text-success m-3">Success</span>'
+
+    }
+
+
+}
+
+
+
